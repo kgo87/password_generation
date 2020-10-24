f9fa3b4dba1fa5baf01116243f92309bda8c70c3
Kat Gomozova <kathy.gomozova@gmail.com>
2020-10-23 19:03:54 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# password_generation
+# Password_Generation
+This repository contains the solution for the homework assignment for unit 3 of UW Coding Bootcamp. 
+
+# Task
+The goal of the task was to develop a code to generate a password based on the user-provided information.
+The starter code (.html, .css and partially .js) was provided. The following modifications were expected:
+
+* When the user clicks the "Generate Password" button, they can select the length of the password (between 8 and 128 characters) and types of characters to be used in the password (numbers, lowercase, uppercase, and special characters).
+
+# URL to a live page
+Link to a live page: https://kgo87.github.io/password_generation/  
+
+# Project Summary
+The folder contains  *.html* and  *.css* files and supplemental images.
+## HTML code
+* Perdonalized website title
+* Navigation bar has links to can help to access different elements of the page
+* When click on *Resume* button, the user is redirected to a page with resume
+* Various semantic elements are used for better clarity
+* *Github* and *LinkedIn* buttons redirects to Github and LinkedIn pages respectively.
+* Screenshot of the part of the code is provided below:
+               
+![Screenshot](html_1.png)
+
+
+## CSS code
+* All methods include comments to describe what each method is doing
+* Order of the methods match the sequence in HTML code
+* Created variables for frequently used properties
+* Implemented animated functionality when the user hovers to the elements
+* Added functionality to display elements differently when the screen size changes
+* Screenshot of the part of the code is provided below:
+
+![Screenshot](css_1.png)
+
+# Finished product
+Here are several screenshots showing the functionality of the developed password generator.
+If the user entered the accepted length of password and selected characters to be used, the password generator will produce this output:
+![Screenshot](working_pw.png)
+If the user entered the incorrect value for the length of password, the page will produce this output:
+![Screenshot](working_pw2.png)
+If the user did not select any group of symbols the page will produce this output:
+![Screenshot](working_pw3.png)
+
+
+
---
+++ script.js
@@ -0,0 +1,120 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// List of numbers:
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+// List of lower case letters:
+var letters_lower_case = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// List of upper case letters:
+var letters_upper_case = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// List of special characters:
+var chars = ["\'\'","\"\",","+",",","_","`","{","|","}","-",".",":","%",")","@","[","","]","^",";","<","=",">","?","*","!","#","$","&","(","~"]
+
+
+function generatePassword(){
+  // Prompt a user for a password length
+  var passwordLength = prompt("Please enter the number of characters for your password. The number should be between 8 and 128.");
+  // if passwordLength doesn't match the length range, then alert the user that they need to provide a correct length
+  if (!(passwordLength >=8 ||passwordLength <=128)){
+    alert("Wrong number of characters. Please try again.")
+    return;
+  }
+
+  
+  // Save user choice for using numbers, letters and characters into variables
+  var isUsingNumbers = confirm("Would you like your password to contain numbers?");
+  var isUsingLowerCase = confirm("Would you like your password to contain lowercase letters?");
+  var isUsingUpperCase = confirm("Would you like your password to contain uppercase letters?");
+  var isUsingSpecialChar = confirm("Would you like your password to contain special characters?");
+  var choices = [isUsingNumbers,isUsingLowerCase,isUsingUpperCase,isUsingSpecialChar];
+
+
+  // Count the number of groups of symbols that user has selected and store that value in a variable
+  function countUserChoices(){
+    var count_choices = 0;
+    if (isUsingNumbers){
+      count_choices+=1;
+    }
+    if (isUsingLowerCase){
+      count_choices+=1;
+    }
+    if (isUsingUpperCase) {
+      count_choices+=1;
+    }
+    if (isUsingUpperCase) {
+      count_choices+=1;
+    }
+    return count_choices;
+  }
+  var numberOfChoices = countUserChoices();
+
+  // Alert the user if they didn't select any group of elements
+  if (numberOfChoices === 0){
+    alert("Please make a selection");
+    return;
+  }
+
+
+  // Calculate the number of elements to be used for a password from each group of symbols
+  var numOfElementsFromEachGroup = Math.floor(passwordLength/numberOfChoices);
+
+  // Ramdomly select the elements from each group of symbols
+  function getElementFromGroup(arr){
+    var selection = [];
+    for (i=0;i<numOfElementsFromEachGroup;i++){
+      selection[i] = arr[Math.floor(Math.random()*arr.length)];
+    }
+    console.log(selection);
+    return selection.join("");
+  }
+
+
+  var password_list = "";
+  // For every selected group of symbols, call the function that randomly selects the symbols and append them to a password list.
+  // Additionally, add the symbols from the selected group to a global list of symbols
+  var globalCombinedListOfElements = [];
+  if (isUsingNumbers){
+    password_list += getElementFromGroup(numbers);
+    globalCombinedListOfElements = [...globalCombinedListOfElements,...numbers];
+  }
+  if (isUsingLowerCase){
+    password_list += getElementFromGroup(letters_lower_case);
+    globalCombinedListOfElements = [...globalCombinedListOfElements,...letters_lower_case];
+  }
+  if (isUsingUpperCase){
+    password_list += getElementFromGroup(letters_upper_case);
+    globalCombinedListOfElements = [...globalCombinedListOfElements,...letters_upper_case];
+  }
+  if (isUsingSpecialChar){
+    password_list += getElementFromGroup(chars);
+    globalCombinedListOfElements = [...globalCombinedListOfElements,...chars];
+  }
+  // console.log(password_list);
+
+  // For odd value of the password length, add symbols to the password from the global list of symbols to mathc the
+  // user-selected length of the password 
+  while (password_list.length < passwordLength){
+    password_list += globalCombinedListOfElements[Math.floor(Math.random()*globalCombinedListOfElements.length)];
+  }
+  
+  // Shuffle the symbols in the final version of the password
+  var password = password_list.split('').sort(function(){return 0.5-Math.random()}).join('');
+  
+
+  return password;
+
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
